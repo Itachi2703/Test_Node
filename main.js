1089d46ef0df655b7d99fd5b4e77ec2497275911
Itachi2703 <ramonhernandez102703@gmail.com>
2021-03-02 19:42:12 -0600

    function completed

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const methods = require("./prueba")
+const methods = require('./prueba')
 
-methods.suma("op")
+var user = {'name': 'pepe'}
+
+
+function ins(paw, err){
+
+	if(paw){
+		
+		user.pasword = methods.suma('op')
+		console.log(user)
+
+	}
+	if(err) throw err;
+
+}
+ins('op')
